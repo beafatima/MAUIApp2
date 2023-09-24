0d72a05656e93c565bd20ec73b1354dc0ed1c042
beafatima <126442264+beafatima@users.noreply.github.com>
2023-09-24 13:44:41 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIApp2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace MAUIApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ImageButtonClick(object sender, EventArgs e)
+        {
+            
+            if (one.Source == one.Source)
+            {
+                one.Source = "reality_1.jpg";
+            }
+            else 
+            {
+                one.Source = "expectation_1.jpg";
+            }
+        }
+        private void ImageButtonClick2(object sender, EventArgs e) 
+        {
+            if (two.Source == two.Source)
+            {
+                two.Source = "reality_2.jpg";
+            }
+            else
+            {
+                two.Source = "expectation_2.jpg";
+            }  
+        } 
+    }
+}
